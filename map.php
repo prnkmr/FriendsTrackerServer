131a548b5c75b3a5ff267dc2ebc37ff63eb4130a
Abijith Krishna <abijitharc@gmail.com>
2015-07-02 14:33:53 +0530

    Commit prob

--- map.php
+++ map.php
@@ -69,20 +69,11 @@
 
         google.maps.event.addDomListener(window, 'load', initialize);
     </script>
-    <style>
-        html,body{
-            height:100%;
-            margin:0px;
-        }
-        #googleMap{
-           min-height:100%;
-        }
-    </style>
 
 </head>
 
 <body>
-<div id="googleMap" style="width:100%;"></div>
+<div id="googleMap" style="width:500px;height:380px;"></div>
 </body>
 <script type="text/javascript">
 
